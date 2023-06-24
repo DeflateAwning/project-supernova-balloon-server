426ad13884af1928c6784820f9e06857317454aa
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-06-24 00:40:28 -0600

    Move files, continue setup

--- supernova/supernova/settings.py
+++ supernova/supernova/settings.py
@@ -11,16 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    SECRET_KEY=(str,),
+    DB_DATABASE=(str, ''),
+    DB_USER=(str, ''),
+    DB_PASSWORD=(str, ''),
+    DB_HOST=(str, ''),
+    DB_PORT=(int, 3050),
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "env('SECRET_KEY'')"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,15 +85,21 @@ WSGI_APPLICATION = 'supernova.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-	# 'default': {
-	# 	'ENGINE': 'django.db.backends.sqlite3',
-	# 	'NAME': BASE_DIR / 'db.sqlite3',
-	# }
 	'default': {
-        'ENGINE': 'firebird',
-        'NAME':
-        
-	}
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	},
+    
+	# TODO make it work with firebird (currently no Django 4 support)
+	# 'default': {
+	# 	'ENGINE': 'django.db.backends.firebird',
+    #     #'ENGINE': 'firebird',
+	# 	'NAME': env('DB_DATABASE'),
+	# 	'USER': env('DB_USER'),
+	# 	'PASSWORD': env('DB_PASSWORD'),
+	# 	'HOST': env('DB_HOST'),
+	# 	'PORT': env('DB_PORT'),
+	# }
 }
 
 

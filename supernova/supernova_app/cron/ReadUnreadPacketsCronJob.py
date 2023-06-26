eaa4edb8b56f8dc38a7860015d8add2ccb4fdcc4
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-06-25 22:48:08 -0600

    Add PacketType1 parser and model

--- supernova/supernova_app/cron/ReadUnreadPacketsCronJob.py
+++ supernova/supernova_app/cron/ReadUnreadPacketsCronJob.py
@@ -1,13 +1,43 @@
 from django_cron import CronJobBase, Schedule
-from ..models import PacketEvent
+from ..models import PacketEvent, PacketType1
+from loguru import logger as llogger
+from tqdm import tqdm
 
 class ReadUnreadPacketsCronJob(CronJobBase):
-	RUN_EVERY_MINS = 1 # every 1 minute
+	RUN_EVERY_MINS = 0.25
 
 	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-	code = 'supernova_app.ReadUnreadPacketsCronJob'    # a unique code
+	code = 'supernova_app.ReadUnreadPacketsCronJob' # a unique code
 
+	@llogger.catch
 	def do(self):
 		print("Running ReadUnreadPacketsCronJob...")
-		...
+		
+		while 1:
+			# get all PacketEvent objects that aren't yet PacketType1 objects
+			# and create PacketType1 objects from them
+			
+			packet_events = PacketEvent.objects.filter(packet_type_1__isnull=True)[:100]
 
+			# TODO add a case where it re-calculates if the struct's string hash changed (i.e., different decode schema)
+
+			if len(packet_events) == 0:
+				#llogger.info(f"End of updates.")
+				break # all done
+
+			llogger.info(f"Read ({len(packet_events)}) PacketEvent objects that aren't yet PacketType1 objects")
+
+			for packet_event in tqdm(packet_events):
+				# Create a new PacketType1 object based on PacketEvent data
+				packet_type_1 = PacketType1.construct_from_base64(
+					packet_event.payload,
+					request_uuid=packet_event,
+					request_timestamp_utc=packet_event.request_timestamp_utc,
+					request_helium_integration_details=packet_event.request_helium_integration_details,
+				)
+
+				# Associate the PacketType1 object with the PacketEvent
+				packet_type_1.request_uuid = packet_event
+
+				# Save the PacketType1 object
+				packet_type_1.save()
--- supernova/supernova_app/models.py
+++ supernova/supernova_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import uuid
+import cstruct
+import base64
+import datetime
 
 class RawRequestLog(models.Model):
 	request_uuid = models.UUIDField(primary_key=True)
@@ -51,7 +54,7 @@ class PacketEventHotspot(models.Model):
 	# one-to-many relationship with PacketEvent (one packet can have many hotspots)
 	request_uuid = models.ForeignKey(PacketEvent, on_delete=models.CASCADE, db_column='request_uuid')
 	
-	hotspot_num_within_packet = models.IntegerField(null=False)
+	hotspot_num_within_packet = models.IntegerField(null=False) # starts at 0
 
 	channel = models.IntegerField()
 	frequency = models.FloatField()
@@ -71,3 +74,111 @@ class PacketEventHotspot(models.Model):
 
 		# set a unique key
 		unique_together = (('request_uuid', 'hotspot_num_within_packet'),)
+
+		# TODO add a constrant to ensure hotspot_num_within_packet < PacketEvent.hotspot_count
+
+class PacketType1(models.Model):
+	request_uuid = models.OneToOneField(PacketEvent, primary_key=True, on_delete=models.CASCADE, db_column='request_uuid', related_name='packet_type_1')
+	request_timestamp_utc = models.DateTimeField()
+	request_helium_integration_details = models.CharField(max_length=255, null=True)
+
+	packet_type = models.PositiveIntegerField()
+	millis_since_boot = models.PositiveIntegerField()
+	packet_seq_num = models.PositiveIntegerField()
+	lora_dr_value = models.PositiveIntegerField()
+
+	themistor_1_temperature_c = models.SmallIntegerField()
+	themistor_2_temperature_c = models.SmallIntegerField()
+	dht_temperature_c = models.SmallIntegerField()
+	dht_humidity_pct = models.SmallIntegerField()
+	bmp_pressure_pa = models.FloatField()
+	bmp_temperature_c = models.FloatField()
+	battery_voltage_mv = models.PositiveIntegerField()
+	mcu_internal_temperature_c = models.SmallIntegerField()
+	is_switch_rtf = models.BooleanField()
+
+	gps_latitude_degrees = models.FloatField()
+	gps_longitude_degrees = models.FloatField()
+	gps_altitude_m = models.FloatField()
+	gps_speed_m_per_s = models.FloatField()
+	gps_course_degrees = models.FloatField()
+
+	gps_satellite_count = models.PositiveIntegerField()
+	gps_hdop_m = models.IntegerField()
+
+	gps_fix_date_epoch_time_sec = models.PositiveIntegerField()
+	gps_fix_date_age_ms = models.PositiveIntegerField()
+
+	class Meta:
+		db_table = 'supernova_packet_type_1'
+
+	@classmethod
+	def construct_from_base64(cls, base64_str: str, request_uuid: PacketEvent, request_timestamp_utc: datetime.datetime, request_helium_integration_details: str):
+		"""
+		Constructs a PacketType1 object from a hex string.
+		"""
+
+		class DataPacket1CStruct(cstruct.MemCStruct):
+			__def__ = """
+			
+			struct {
+				uint8_t packet_type; // always 1 for this struct
+				uint32_t millis_since_boot;
+				uint16_t packet_seq_num;
+				uint8_t lora_dr_value;
+
+				int8_t themistor_1_temperature_c;
+				int8_t themistor_2_temperature_c;
+				int8_t dht_temperature_c;
+				int8_t dht_humidity_pct;
+				float bmp_pressure_pa;
+				float bmp_temperature_c;
+				uint16_t battery_voltage_mv;
+				int8_t mcu_internal_temperature_c;
+				uint8_t is_switch_rtf; // TODO pack other bools in here, if we want
+
+				// HERE is a good split if we ever wanted 2 different types of packets
+				int32_t gps_latitude_degrees_x1e6;
+				int32_t gps_longitude_degrees_x1e6;
+				int32_t gps_altitude_cm;
+				int32_t gps_speed_100ths_of_knot;
+				int32_t gps_course_100ths_of_degree;
+				uint16_t gps_satellite_count;
+				int32_t gps_hdop_cm;
+				uint32_t gps_fix_date_epoch_time_sec;
+				uint32_t gps_fix_date_age_ms;
+			};
+			"""
+
+		data = base64.b64decode(base64_str)
+		packet = DataPacket1CStruct()
+		packet.unpack(data)
+
+		return cls(
+			request_uuid=request_uuid,
+			request_timestamp_utc=request_timestamp_utc,
+			request_helium_integration_details=request_helium_integration_details,
+			packet_type=packet.packet_type,
+			millis_since_boot=packet.millis_since_boot,
+			packet_seq_num=packet.packet_seq_num,
+			lora_dr_value=packet.lora_dr_value,
+			themistor_1_temperature_c=packet.themistor_1_temperature_c,
+			themistor_2_temperature_c=packet.themistor_2_temperature_c,
+			dht_temperature_c=packet.dht_temperature_c,
+			dht_humidity_pct=packet.dht_humidity_pct,
+			bmp_pressure_pa=packet.bmp_pressure_pa,
+			bmp_temperature_c=packet.bmp_temperature_c,
+			battery_voltage_mv=packet.battery_voltage_mv,
+			mcu_internal_temperature_c=packet.mcu_internal_temperature_c,
+			is_switch_rtf=packet.is_switch_rtf,
+			gps_latitude_degrees=packet.gps_latitude_degrees_x1e6 / 1e6,
+			gps_longitude_degrees=packet.gps_longitude_degrees_x1e6 / 1e6,
+			gps_altitude_m=packet.gps_altitude_cm / 100,
+			gps_speed_m_per_s=packet.gps_speed_100ths_of_knot / 100 * 0.514444444, # knots to m/s
+			gps_course_degrees=packet.gps_course_100ths_of_degree / 100,
+			gps_satellite_count=packet.gps_satellite_count,
+			gps_hdop_m=packet.gps_hdop_cm / 100,
+			gps_fix_date_epoch_time_sec=packet.gps_fix_date_epoch_time_sec,
+			gps_fix_date_age_ms=packet.gps_fix_date_age_ms,
+		)
+	
